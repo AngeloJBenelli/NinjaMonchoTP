6a4b78bdbcf16d8a124a37cf77e35b575ed618fc
Angelo J. Benelli <angelojbenelli@gmail.com>
2025-05-10 03:47:47 -0300

    cambios

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 import HelloWorldScene from "./scenes/HelloWorldScene.js";
+import GameScene from "./scenes/GameScene.js";
+import WinScene from "./scenes/WinScene.js";
+import LoseScene from "./scenes/LoseScene.js";
 
 // Create a new Phaser config object
 const config = {
@@ -27,8 +30,9 @@ const config = {
   // List of scenes to load
   // Only the first scene will be shown
   // Remember to import the scene before adding it to the list
-  scene: [HelloWorldScene],
+  scene: [HelloWorldScene, GameScene, WinScene, LoseScene],
 };
 
 // Create a new Phaser game instance
 window.game = new Phaser.Game(config);
+
