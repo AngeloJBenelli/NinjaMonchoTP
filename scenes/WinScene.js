042839dc53836bdada1f17bbdcd60f94161c6dc5
Angelo J. Benelli <angelojbenelli@gmail.com>
2025-05-10 03:51:34 -0300

    cambios

---
+++ scenes/WinScene.js
@@ -0,0 +1,43 @@
+// URL to explain PHASER scene: https://rexrainbow.github.io/phaser3-rex-notes/docs/site/scene/
+
+export default class WinScene extends Phaser.Scene {
+  constructor() {
+    super("win");
+  }
+
+  init(data) {
+    this.score = data.score || 0;
+  }
+
+  preload() {
+    this.load.image("sky", "./public/assets/FondoMenu.jpg");
+  }
+
+  create() {
+    this.add.image(400, 300, "sky");
+    this.add.rectangle(400, 300, 800, 600, 0x000000, 0.6);
+
+    this.add.text(400, 200, "¡GANASTE!", {
+      fontSize: "64px",
+      fill: "#00ff00",
+    }).setOrigin(0.5);
+
+    this.add.text(400, 300, `Puntos: ${this.score}`, {
+      fontSize: "32px",
+      fill: "#ffffff",
+    }).setOrigin(0.5);
+
+    this.add.text(400, 400, "Presiona R para reiniciar", {
+      fontSize: "24px",
+      fill: "#ffffff",
+    }).setOrigin(0.5);
+
+    this.rKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+  }
+
+  update() {
+    if (Phaser.Input.Keyboard.JustDown(this.rKey)) {
+      this.scene.start("game");
+    }
+  }
+}
