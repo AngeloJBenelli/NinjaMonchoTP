60e39a230d97f0fac144fff065233d3ac714637c
Angelo J. Benelli <angelojbenelli@gmail.com>
2025-05-11 20:27:16 -0300

    cambios

--- scenes/WinScene.js
+++ scenes/WinScene.js
@@ -11,23 +11,26 @@ export default class WinScene extends Phaser.Scene {
 
   preload() {
     this.load.image("sky", "./public/assets/FondoMenu.jpg");
+    this.load.image("winner", "./public/assets/Ganador.png");
   }
 
   create() {
     this.add.image(400, 300, "sky");
     this.add.rectangle(400, 300, 800, 600, 0x000000, 0.6);
 
-    this.add.text(400, 200, "¡GANASTE!", {
+    this.add.image(400, 200, "winner").setScale(0.19).setOrigin(0.5, 0.3);
+
+    this.add.text(400, 50, "¡GANASTE!", {
       fontSize: "64px",
       fill: "#00ff00",
     }).setOrigin(0.5);
 
-    this.add.text(400, 300, `Puntos: ${this.score}`, {
+    this.add.text(400, 500, `Puntos: ${this.score}`, {
       fontSize: "32px",
       fill: "#ffffff",
     }).setOrigin(0.5);
 
-    this.add.text(400, 400, "Presiona R para reiniciar", {
+    this.add.text(400, 550, "Presiona R para reiniciar", {
       fontSize: "24px",
       fill: "#ffffff",
     }).setOrigin(0.5);
