0e2b89b8d90e46554169f29677d81dbb0e330366
Angelo J. Benelli <angelojbenelli@gmail.com>
2025-05-11 20:28:14 -0300

    cambios

--- scenes/LoseScene.js
+++ scenes/LoseScene.js
@@ -11,23 +11,26 @@ export default class LoseScene extends Phaser.Scene {
 
   preload() {
     this.load.image("sky", "./public/assets/FondoMenu.jpg");
+    this.load.image("gameover", "./public/assets/Derrotado.png");
   }
 
   create() {
     this.add.image(400, 300, "sky");
     this.add.rectangle(400, 300, 800, 600, 0x000000, 0.6);
 
-    this.add.text(400, 200, "¡PERDISTE!", {
+    this.add.image(400, 200, "gameover").setScale(0.4).setOrigin(0.5, 0.32);
+
+    this.add.text(400, 100, "¡PERDISTE!", {
       fontSize: "64px",
       fill: "#ff0000",
     }).setOrigin(0.5);
 
-    this.add.text(400, 300, `Puntos: ${this.score}`, {
+    this.add.text(400, 500, `Puntos: ${this.score}`, {
       fontSize: "32px",
       fill: "#ffffff",
     }).setOrigin(0.5);
 
-    this.add.text(400, 400, "Presiona R para reiniciar", {
+    this.add.text(400, 550, "Presiona R para reiniciar", {
       fontSize: "24px",
       fill: "#ffffff",
     }).setOrigin(0.5);
